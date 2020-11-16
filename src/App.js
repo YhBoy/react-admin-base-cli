dd2b42523feb10a7005fc05000a9465da3117986
yanghao <yanghao@weiye360.cn>
2020-11-16 17:44:04 +0800

    项目文件初始化

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button } from 'antd'
+import 'antd/dist/antd.css';
+
+import Login from './pages/Login/Login'
+import Admin from './pages/Admin/Admin'
+
+import { BrowserRouter,Route,Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/login" component={Login} />
+                <Route path="/admin" component={Admin} />
+            </Switch> 
+        </BrowserRouter> 
     </div>
   );
 }
